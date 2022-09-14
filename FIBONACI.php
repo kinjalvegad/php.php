c6b0bf12c5be8f4e69e659ce0ba5503f263bffb2
kinjalvegad <110524939+kinjalvegad@users.noreply.github.com>
2022-09-14 17:45:07 +0530

    Add files via upload

---
+++ FIBONACI.php
@@ -0,0 +1,18 @@
+//1A,2B,3C,4D//
+<?php 
+   if(isset($_POST['txtno']))
+   {
+	   $a=65;
+	   $no=$_POST['txtno'];
+	   for($i=1;$i<=$no;$i++)
+	   { 
+         echo $i.chr($a)."&nbsp";
+		 $a++;
+	   }
+   }
+ ?>
+		<form action="FIBONACI.php"method="post">
+			<input type="text"name="txtno"><br><br>
+			<input type="submit"value="display">
+	</form>		
+		
